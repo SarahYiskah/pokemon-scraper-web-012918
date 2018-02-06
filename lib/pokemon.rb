f589586339e7388b352baa25076fd28eb2133b4b
github username <github email address>
2018-02-06 12:00:41 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
+require 'pry'
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+  def self.save(name, type, database)
+    # database = SQLite3::Database.new("/Users/michael/catalog.db")
+    sql = <<-SQL
+      INSERT INTO pokemon (name,type) VALUES (?,?)
+    SQL
+    database.execute(sql,name,type)
+  end
+  def self.find(id, database)
+    # binding.pry
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = (?)
+      SQL
+    response = database.execute(sql, id)[0]
+    Pokemon.new(name: response[1], type: response[2],db:  database, id: response[0])
+  end
+  # private
+  # def self.format_sql(response)
+  #   Pokemon.new(response[1], response[2], @db, response[0])
+  # end
+
 end
